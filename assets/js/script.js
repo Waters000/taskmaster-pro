6600e3d497edaf44e9adf28d19879758c1b94f37
Chris Waters <floridarealty001@gmail.com>
2022-01-19 15:41:28 -0500

    updated styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -109,7 +109,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -162,16 +162,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+   $(this).addClass("dropover");
+   $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", event.target);
-  },
+    $(event.target).addClass("dropover-active");
+    },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).addClass("dropover-active");
   },
   update: function(event) {
     var tempArr = [];
@@ -210,16 +212,25 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
    ui.draggable.remove();
+   $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
     console.log(ui);
+    $(".bottom-trash").addClass("bottom-trash-active");
+
   },
   out: function(event, ui) {
     console.log(ui);
+    $(".bottom-trash").removeClass("bottom-trash-active");
+
   }
 });
 
-
+setInterval(function() {
+ $(".card .list-group-item").each(function(index, el){
+   auditTask(el);
+ })
+}, 1800000);
 // load tasks for the first time
 loadTasks();
 
